a41c9ece893fcd5fc577681ef8122073b7f1b20c
RoboZoom <56803986+RoboZoom@users.noreply.github.com>
2023-12-02 06:46:52 -0700

    Reorg

--- lib/day_2.ex
+++ lib/day_2.ex
@@ -1,84 +1,9 @@
-defmodule Week1 do
-  def day_one_a() do
-    File.stream!("assets/day_one_input.txt")
-    |> Enum.map(&get_line_nums/1)
-    |> Enum.sum()
-  end
-
-  def day_one_b() do
-    # File.stream!("assets/day_one_input.txt")
-    File.stream!("assets/test.txt")
-    |> Enum.map(&get_line_nums_spelled/1)
-    |> Enum.sum()
-  end
-
-  defp get_line_nums(str) when is_binary(str) do
-    num_list =
-      Regex.scan(~r"\d", str)
-      |> List.flatten()
-
-    {f, _} = List.first(num_list) |> Integer.parse()
-    {l, _} = List.last(num_list) |> Integer.parse()
-
-    f * 10 + l
-  end
-
-  def get_line_nums_spelled(str) when is_binary(str) do
-    regex_match = ~r"(?=(\d|one|two|three|four|five|six|seven|eight|nine))"
-
-    num_list =
-      Regex.scan(regex_match, str)
-      |> Enum.map(&List.last/1)
-      |> List.flatten()
-
-    f = List.first(num_list) |> number_from_match()
-    l = List.last(num_list) |> number_from_match()
-
-    f * 10 + l
-  end
-
-  defp number_from_match(x) do
-    case x do
-      "one" ->
-        1
-
-      "two" ->
-        2
-
-      "three" ->
-        3
-
-      "four" ->
-        4
-
-      "five" ->
-        5
-
-      "six" ->
-        6
-
-      "seven" ->
-        7
-
-      "eight" ->
-        8
-
-      "nine" ->
-        9
-
-      _ ->
-        {y, _} = Integer.parse(x)
-        y
-    end
-  end
-
+defmodule Day2 do
   def day_two_a() do
     # File.stream!("assets/test.txt")
     File.stream!("assets/day_two_input.txt")
     |> Enum.map(&parse_game/1)
-    |> IO.inspect(label: "Games")
     |> Enum.map(&check_rgb_param(&1, 12, 14, 13))
-    |> IO.inspect(label: "Validity")
     |> Enum.reduce(0, fn {id, valid}, acc ->
       case valid do
         false -> acc
@@ -91,13 +16,11 @@ defmodule Week1 do
     # File.stream!("assets/test.txt")
     File.stream!("assets/day_two_input.txt")
     |> Enum.map(&parse_game/1)
-    |> IO.inspect(label: "Games")
     |> Enum.map(&get_power/1)
-    |> IO.inspect(label: "Power")
     |> Enum.sum()
   end
 
-  def get_power({id, games}) do
+  def get_power({_id, games}) do
     [:red, :green, :blue]
     |> Enum.map(&local_max(games, &1))
     |> Enum.reduce(1, &(&1 * &2))
