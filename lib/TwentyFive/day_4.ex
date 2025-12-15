b31ea287b7603c9c15c14fa2907cba99ae548503
RoboZoom <56803986+RoboZoom@users.noreply.github.com>
2025-12-15 13:09:04 -0300

    Updated algorithm. Added input files.

--- lib/TwentyFive/day_4.ex
+++ lib/TwentyFive/day_4.ex
@@ -2,59 +2,73 @@ defmodule TwentyFive.Day4 do
   def problem_one(test_data \\ false) do
     fetch_data(test_data)
     |> parse_data()
-    |> Enum.map(&max_joltage(&1, 2, ""))
+    |> create_grid()
     |> Enum.sum()
   end
 
   def problem_two(test_data \\ false) do
     fetch_data(test_data)
     |> parse_data()
-    |> Enum.map(&max_joltage(&1, 12, ""))
-    |> Enum.sum()
+
+    # |> Enum.map(&max_joltage(&1, 12, ""))
+    # |> Enum.sum()
   end
 
   def parse_data(input_stream) do
     input_stream
     |> String.trim()
     |> String.split("\n")
+  end
+
+  def create_grid(starter_grid) do
+    starter_grid
     |> Enum.map(&String.graphemes/1)
-    |> Enum.map(
-      &Enum.map(&1, fn el ->
-        {i, _} = Integer.parse(el)
-        i
-      end)
-    )
-  end
-
-  def max_joltage(_batteries_bank, 0, acc) do
-    {i, _} = Integer.parse(acc)
-    iv
-  end
-
-  def max_joltage(batteries_bank, length, acc) do
-    # IO.puts("Max Joltage: #{batteries_bank} - #{length}")
-
-    available_batteries =
-      case length do
-        1 ->
-          batteries_bank
-
-        _ ->
-          {useable_batteries, _reserved_batteries} = Enum.split(batteries_bank, -(length - 1))
-          useable_batteries
-      end
-
-    {max_num, index} =
-      available_batteries
+    |> Enum.with_index()
+    |> Enum.map(fn {line, y_index} ->
+      line
       |> Enum.with_index()
-      |> Enum.max_by(fn {val, _index} ->
-        val
+      |> Enum.map(fn {sym, x_index} ->
+        {sym, 0, {x_index, y_index}}
       end)
+    end)
+    |> IO.inspect(label: "Starter Grid")
 
-    result = acc <> Integer.to_string(max_num)
-    {_front, back} = Enum.split(batteries_bank, index + 1)
+    # |> Enum.reduce()
+  end
 
-    max_joltage(back, length - 1, result)
+  def populate_grid(grid, {_x, y} = _coord, _max_x, max_y) when y >= max_y, do: grid
+
+  def populate_grid(grid, {x, y} = _coord, max_x, max_y) when x >= max_x,
+    do: populate_grid(grid, {0, y + 1}, max_x, max_y)
+
+  def populate_grid(grid, {x, y} = coord, max_x, max_y) do
+    coord
+    |> get_adjacent_grid_coords(max_x, max_y)
+    |> Enum.reduce()
+  end
+
+  defp get_adjacent_grid_coords({x, y} = coord, max_x, max_y) do
+    [
+      {x + 1, y},
+      {x + 1, y - 1},
+      {x + 1, y + 1},
+      {x, y + 1},
+      {x, y - 1},
+      {x - 1, y},
+      {x - 1, y - 1},
+      {x - 1, y + 1}
+    ]
+    |> Enum.filter(&check_valid_coord(&1, max_x, max_y))
+  end
+
+  defp check_valid_coord({x, y} = coord, max_x, max_y) do
+    cond do
+      x >= max_x -> false
+      x < 0 -> false
+      y >= max_y -> false
+      y < 0 -> false
+      true -> true
+    end
   end
 
   defp fetch_data(test) when test == false, do: File.read!("assets/2025/d04/compute_input.txt")
